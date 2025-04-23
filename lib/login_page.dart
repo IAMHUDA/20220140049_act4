aedc48d092ae6dcfba08f142a1f688b29341fcc1
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-23 18:50:35 +0700

    add elevatedbutton to homepage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -48,6 +48,16 @@ class _LoginPageState extends State<LoginPage> {
                   return null;
                 },
               ),
+              ElevatedButton(
+                onPressed: (){
+                  if (_formKey.currentState!.validate()){
+                    Navigator.pushReplacement(context, 
+                    MaterialPageRoute(builder: (context) => const HomePage()));
+                  }
+
+                }
+              , child: Text('Login')
+              ),
             ],
           ),)),
     );
